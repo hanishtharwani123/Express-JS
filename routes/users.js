7db5e18d76c0528920fe6bb72f80cdd1513afc4b
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-06-09 10:51:31 +0530

    change commit

--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,43 @@ router.get("/new", (req, res) => {
   res.send("new user");
 });
 
-router.post("/", (req, res) => {
-  res.send("create user");
-});
-
 router.get("/:id", (req, res) => {
   res.send(`Get User with ID ${req.params.id}`);
+});
+
+router.post("/", (req, res) => {
+  res.send("Create user");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`Get user with ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`update user with ID ${req.params.id}`);
+  })
+  .delete("/:id", (req, res) => {
+    res.send(`delete user with ID ${req.params.id}`);
+  });
+
+// router.get("/:id", (req, res) => {
+//   res.send(`Get user with ID ${req.params.id}`);
+// });
+
+// router.put("/:id", (req, res) => {
+//   res.send(`update user with ID ${req.params.id}`);
+// });
+
+// router.delete("/:id", (req, res) => {
+//   res.send(`delete user with ID ${req.params.id}`);
+// });
+
+const users = [{ name: "Kyle" }, { name: "Sally" }];
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
 });
 
 module.exports = router;
